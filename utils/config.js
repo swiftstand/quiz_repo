28d6ce98f0831a0886854ee965baae396033f9e7
swiftstand <officialswiftstand@gmail.com>
2024-07-27 09:35:20 +0100

    deployed

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,7 @@ export const DB_CREDENTIALS= {
     // user: 'root',
     // password: 'Hammedswift23.',
     // database: 'quiz_db'
-    host: 'localhost',
+    host: 'localhostL:3306',
     user: 'learnfir_manager',
     password: 'KO{F&1iNVl!A',
     database: 'learnfir_quiz_db'
